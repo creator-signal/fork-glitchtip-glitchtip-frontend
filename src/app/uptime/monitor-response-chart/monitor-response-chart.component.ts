5d82bf400b78513b5687e3c90801d3c00dc0252e
James Kiger <james.kiger@gmail.com>
2025-06-17 09:14:37 -0400

    Not for merging, test set up for chart.js

--- src/app/uptime/monitor-response-chart/monitor-response-chart.component.ts
+++ src/app/uptime/monitor-response-chart/monitor-response-chart.component.ts
@@ -12,13 +12,29 @@ import {
 } from "@angular/core";
 import { debounceTime, fromEvent, Subscription } from "rxjs";
 import { ResponseTimeSeries } from "../uptime.interfaces";
-import { NgxChartsModule } from "@swimlane/ngx-charts";
+import {
+  Chart,
+  Colors,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Legend
+} from 'chart.js'
+Chart.register(
+  Colors,
+  BarController,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Legend
+);
+
 
 @Component({
   selector: "gt-monitor-response-chart",
   templateUrl: "./monitor-response-chart.component.html",
   styleUrls: ["./monitor-response-chart.component.scss"],
-  imports: [NgxChartsModule],
 })
 export class MonitorResponseChartComponent
   implements AfterViewInit, OnInit, OnDestroy
@@ -32,7 +48,7 @@ export class MonitorResponseChartComponent
     xScaleMin: Date;
   };
 
-  @ViewChild("containerRef") containerRef?: ElementRef;
+  @ViewChild("canvasRef") canvasRef?: ElementRef<HTMLCanvasElement>;
   //Ngx-charts does not do continuous resizing, but only adjusts after window resizing completes.
   //This is a workaround to force continous resizing
   @HostListener("window:resize")
@@ -57,13 +73,37 @@ export class MonitorResponseChartComponent
   }
 
   ngAfterViewInit(): void {
-    this.resizeChart();
+    const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+    if (this.canvasRef) {
+      console.log("container ref")
+      new Chart(this.canvasRef.nativeElement, {
+        type: "bar",
+        data: {
+          labels: data.map((row) => row.year),
+          datasets: [
+            {
+              label: "Acquisitions by year",
+              data: data.map((row) => row.count),
+            },
+          ],
+        },
+      });
+    }
   }
 
   resizeChart() {
-    if (this.containerRef) {
-      this.view = [this.containerRef.nativeElement.offsetWidth, 250];
-    }
+    // if (this.containerRef) {
+    //   this.view = [this.containerRef.nativeElement.offsetWidth, 250];
+    // }
   }
 
   ngOnDestroy(): void {
