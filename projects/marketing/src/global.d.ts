f8dfb92bc76d7aad2bbbff102dea9a733f25c32d
Woodsii <beanmanwoods@gmail.com>
2026-06-25 13:58:20 -0400

    add license to chatwoot conversation

---
+++ projects/marketing/src/global.d.ts
@@ -0,0 +1,10 @@
+`In order to access the chatwoot SDK without TS throwing an error, 
+this snippet is required. It adds chatwoot as a property to the Window.`
+
+export {};
+
+declare global {
+  interface Window {
+    $chatwoot?: any; // Use 'any' or provide a more specific type if known
+  }
+}
